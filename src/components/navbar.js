806e009fec6c7bab74d48aa61a7202fd31a547fd
Abdqadr1 <abolarinwaquadri@gmail.com>
2022-05-11 21:25:55 +0800

    change navbar text

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,20 +12,13 @@ class NavBar extends React.Component{
         return (
             <Navbar bg="light" expand="md">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Bank Api</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="w-100 me-auto text-center justify-content-md-end">
                             <Link role="button" className='nav-link' tabIndex={0} to="/banks">Banks</Link>
                             <Link role="button" className='nav-link' tabIndex={0} to="/system">System Dashboard</Link>
                             <Link role="button" className='nav-link text-danger' tabIndex={0} to="/logout">Log out</Link>
-                            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown> */}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
